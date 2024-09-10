0c9559f8edde7172469e0a289663f4d83e0fbdf6
RNabila <nhr6292@gmail.com>
2024-09-10 08:48:44 -0400

    Create CFU1.py

---
+++ CFU1.py
@@ -0,0 +1,13 @@
+
+
+fname= input("Your first name")
+lname= input("Your last name")
+color= input("Favorite color")
+subject= input ("What are you learning?")
+show= input("Favorite show")
+
+print ("Hello " + fname, lname)
+print ("Your favorite color is " + color + " and favorite show is " + show)
+print ("The topic you are learning is " + subject)
+
+

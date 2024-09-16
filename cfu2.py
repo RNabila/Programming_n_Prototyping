98ce1263bbc0ee74d52657043854f934168bae19
RNabila <nhr6292@gmail.com>
2024-09-16 09:18:52 -0400

    Create cfu2.py

---
+++ cfu2.py
@@ -0,0 +1,6 @@
+fname= input("What is your first name?")
+lname= input("What is your last name?")
+age= input("How old are you?")
+read= input("What age did you start reading?")
+
+print(fname + " " + lname + " is " + str(age) + " years old, and \nhas been reading since she was " + str(read) + " years old.")

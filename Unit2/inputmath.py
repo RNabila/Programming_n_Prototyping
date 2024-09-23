1930785963fabd1b84cb2db9421e0d626894e96b
RNabila <nhr6292@gmail.com>
2024-09-23 09:19:55 -0400

    Update inputmath.py

--- Unit2/inputmath.py
+++ Unit2/inputmath.py
@@ -3,3 +3,9 @@ r= int(input("what is the radius of your circle?"))
 c= 2*r*math.pi
 a= math.pi*r**2
 print("C=" ,c, " A=", a)
+
+
+r= float(input("what is the radius of your cylinder?"))
+height= int(input("What is the height of your cylinder?"))
+v= math.pi*r**2*height
+print("Volume=" ,v)

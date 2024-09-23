86e160a38165db1dc186167a438c154700054b79
RNabila <nhr6292@gmail.com>
2024-09-23 09:13:34 -0400

    Update inputmath.py

--- Unit2/inputmath.py
+++ Unit2/inputmath.py
@@ -1,5 +1,5 @@
 import math
 r= int(input("what is the radius of your circle?"))
-C= 2*r*math.pi
-A= math.pi*r**2
-print("C=" ,C, " A=", A)
+c= 2*r*math.pi
+a= math.pi*r**2
+print("C=" ,c, " A=", a)

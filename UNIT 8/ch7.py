8a371749bb098f1ab667441b4f012dfdef25015c
RNabila <nhr6292@gmail.com>
2025-03-14 08:24:51 -0400

    Create ch7.py

---
+++ UNIT 8/ch7.py
@@ -0,0 +1,36 @@
+import turtle
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    t.forward(length * n) 
+    t.left(angle) 
+    draw(t, length, n-1) 
+    t.right(2 * angle)
+    draw(t, length, n-1) 
+    t.left(angle) 
+    t.backward(length * n) 
+
+def square(t):
+   
+    for _ in range(4):
+        t.forward(100) 
+        t.left(90) 
+
+
+bob = turtle.Turtle()
+
+
+draw(bob, 10, 4)  
+
+#no overlapping
+bob.penup()
+bob.goto(-50, -50) #diff pos
+bob.pendown()
+
+
+square(bob)
+
+
+turtle.done()
